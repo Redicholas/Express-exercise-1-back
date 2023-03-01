a978552f6b281eae2e071f584d30bac18a9ed9cb
Nicholas Peters <peters.nicholas@gmail.com>
2023-03-01 14:44:48 +0100

    add some JSON for front end to display

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
 var express = require("express");
 var router = express.Router();
 
+let users = [
+  { id: 1, name: "John" },
+  { id: 2, name: "Jane" },
+  { id: 3, name: "Jack" },
+  { id: 4, name: "Alice" },
+];
+
 /* GET users listing. */
 router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
+  res.json(users);
 });
 
 router.get("/test", (req, res) => {
