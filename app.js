292900197a76591663c47d777b461715d2fd303f
Nicholas Peters <peters.nicholas@gmail.com>
2023-03-02 09:47:25 +0100

    add ability to add users to array

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
+var cors = require("cors");
+
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var productsRouter = require("./routes/products");
 
 var app = express();
+app.use(cors());
 
 app.use(logger("dev"));
 app.use(express.json());
--- routes/users.js
+++ routes/users.js
@@ -6,11 +6,21 @@ let users = [
   { id: 2, name: "Jane" },
   { id: 3, name: "Jack" },
   { id: 4, name: "Alice" },
+  { id: 5, name: "Bob" },
 ];
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
   res.json(users);
+});
+
+router.post("/", (req, res, next) => {
+  let newUser = req.body;
+  newUser.id = users.length + 1;
+  users.push(newUser);
+  console.log(newUser);
+
+  res.json(newUser);
 });
 
 router.get("/test", (req, res) => {
